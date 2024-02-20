510bc75f3bbb7ea5e1bcd601cd8ddad7fcef39b9
James Le <dohuul@gmail.com>
2024-02-19 16:03:37 -0800

    tests

---
+++ python/tests/service/hello.py
@@ -0,0 +1,11 @@
+number = 7
+
+my_string = "this is my number in string format %d" %(number)
+
+print("%s" %(my_string));
+
+print("test");
+
+file = open("C:\\Users\\qsbtt\\Downloads\\test.txt", "r");
+content = file.read();
+print(content);
